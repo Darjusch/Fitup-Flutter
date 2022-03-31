109f0c52de96e2233797716338c5ba2953aaafcc
Darjusch <Darjusch.schrand@code.berlin>
2022-03-31 11:09:07 +0200

    navigation update

--- lib/utils/navigation_helper.dart
+++ lib/utils/navigation_helper.dart
@@ -5,6 +5,8 @@ import '../screens/bet_overview_screen/bet_overview_screen.dart';
 import '../screens/create_bet_screen/create_bet_screen.dart';
 import '../screens/single_bet_screen/single_bet_screen.dart';
 import '../screens/image_picker_screen/image_picker_screen.dart';
+import '../screens/signup_screen/signup_screen.dart';
+import '../screens/signin_screen/sign_in_screen.dart';
 
 class NavigationHelper {
   void goToBetImagePickerScreen(String docId, BuildContext context) {
@@ -36,5 +38,14 @@ class NavigationHelper {
   void goToBetHistoryScreen(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
         builder: (context) => const BetHistoryScreen()));
+  }
+
+  void goToSignUpScreen(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => SignUpScreen()));
+  }
+  void goToSignInScreen(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => SignInScreen()));
   }
 }
