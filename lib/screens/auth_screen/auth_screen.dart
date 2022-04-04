82ae94da6e18b4fa2bf2d7226e5c131a9630513a
Darjusch <Darjusch.schrand@code.berlin>
2022-04-04 12:41:49 +0200

    password show hide

--- lib/screens/auth_screen/auth_screen.dart
+++ lib/screens/auth_screen/auth_screen.dart
@@ -3,11 +3,23 @@ import 'package:provider/provider.dart';
 
 import '../../providers/AuthenticationService.dart';
 
-class AuthScreen extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key key}) : super(key: key);
 
-  AuthScreen({Key key}) : super(key: key);
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
+  bool _passwordVisible;
+
+  @override
+  void initState() {
+    _passwordVisible = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,44 +29,59 @@ class AuthScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          TextField(
-          controller: emailController,
-          decoration: const InputDecoration(
-            labelText: "Email",
-          ),
-        ),
-        TextField(
-          controller: passwordController,
-          decoration: const InputDecoration(
-            labelText: "Password",
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-
-          ElevatedButton(
-            onPressed: () {
-              context.read<AuthenticationService>().signIn(
-                email: emailController.text.trim(),
-                password: passwordController.text.trim(),
-              );
-            },
-            child: const Text("Sign in"),
-
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(primary: Colors.white),
-            onPressed: () {
-              context.read<AuthenticationService>().signUp(
-                email: emailController.text.trim(),
-                password: passwordController.text.trim(),
-              );
-            },
-            child: const Text("Sign Up", style: TextStyle(color: Colors.blue)),
-          ),
-        ],
-        )
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+              ),
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: !_passwordVisible,
+              decoration: InputDecoration(
+                labelText: "Password",
+                hintText: 'Enter your password',
+                suffixIcon: IconButton(
+                    icon: Icon(
+                      // Based on passwordVisible state choose the icon
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                    onPressed: () {
+                      // Update the state i.e. toogle the state of passwordVisible variable
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    }),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<AuthenticationService>().signIn(
+                          email: emailController.text.trim(),
+                          password: passwordController.text.trim(),
+                        );
+                  },
+                  child: const Text("Sign in"),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.white),
+                  onPressed: () {
+                    context.read<AuthenticationService>().signUp(
+                          email: emailController.text.trim(),
+                          password: passwordController.text.trim(),
+                        );
+                  },
+                  child: const Text("Sign Up",
+                      style: TextStyle(color: Colors.blue)),
+                ),
+              ],
+            )
           ],
         ),
       ),
