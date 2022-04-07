0cdd6c2d490005fc04a6064df8b50f887bbd9aa7
Darjusch <Darjusch.schrand@code.berlin>
2022-04-07 15:09:00 +0200

    Bet model and provider

--- lib/models/bet.dart
+++ lib/models/bet.dart
@@ -20,6 +20,29 @@ class Bet {
     @required this.value,
     @required this.userID,
   });
+}
 
-  
+class BetProvider extends ChangeNotifier {
+  List<Bet> bets = [];
+
+  List<Bet> get getAllBets {
+    return bets;
+  }
+
+  int getNumberOfbets() {
+    return bets.length;
+  }
+
+  void addBet(Bet bet) {
+    debugPrint("BETS LENGTH: ${bets.length}");
+    bets.add(bet);
+    debugPrint("added BET");
+    debugPrint("BETS LENGTH NOW: ${bets.length}");
+    notifyListeners();
+  }
+
+  // Bet getBet() {} for this we need to add a ID into Bet
+
+  // void updateBet() {} for this we need to add a ID into Bet
+
 }
