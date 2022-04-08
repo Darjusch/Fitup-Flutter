d71f3b1ae18dfb3bd6b7c54e952fff8bf200ae50
Darjusch <Darjusch.schrand@code.berlin>
2022-04-08 10:38:18 +0200

    displaying videos in singlebetscreen and changed listview scroll direction to horizontal + changed size of video and images

--- lib/screens/single_bet_screen/single_bet_screen.dart
+++ lib/screens/single_bet_screen/single_bet_screen.dart
@@ -1,5 +1,7 @@
+import 'package:fitup/widgets/video_player_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:video_player/video_player.dart';
 
 class SingleBetScreen extends StatefulWidget {
   final String docId;
@@ -13,7 +15,28 @@ class SingleBetScreen extends StatefulWidget {
 }
 
 class _SingleBetScreenState extends State<SingleBetScreen> {
+  String fileType;
+  String filePath;
+  VideoPlayerController videoController;
   var dateFormat = DateFormat('dd/MM/yyyy, HH:mm');
+
+  @override
+  void initState() {
+    if (widget.data['videos'] != null) {
+      videoController = VideoPlayerController.network(widget.data['videos'][0])
+        ..addListener(() => setState(() {}))
+        ..initialize().then((_) => videoController.play());
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (videoController != null) {
+      videoController.dispose();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,37 +44,55 @@ class _SingleBetScreenState extends State<SingleBetScreen> {
       appBar: AppBar(
         title: const Text("Bet details"),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-              height: 200,
-              width: 200,
-              child: Column(
-                children: [
-                  Text("Action: ${widget.data['action']}"),
-                  Text("Duration: ${widget.data['duration']}"),
-                  Text("Time: ${widget.data['time']}"),
-                  Text("Value: ${widget.data['value']}€"),
-                  Text(
-                      "StartDate: ${dateFormat.format(widget.data['startDate'].toDate())}"),
-                  Text("isActive: ${widget.data['isActive']}"),
-                  Text("success: ${widget.data['success']}"),
-                ],
-              )),
-          widget.data['images'] != null
-              ? SizedBox(
-                  height: 400,
-                  width: 400,
-                  child: ListView(children: [
-                    for (var image in widget.data['images'])
-                      Image.network(
-                        "$image",
-                        width: 100,
-                        height: 100,
-                      )
-                  ]))
-              : const Text("No Images uploaded yet"),
-        ],
+      body: SizedBox(
+        height: 1000,
+        width: 400,
+        child: Column(
+          children: [
+            SizedBox(
+                height: 200,
+                width: 200,
+                child: Column(
+                  children: [
+                    Text("Action: ${widget.data['action']}"),
+                    Text("Duration: ${widget.data['duration']}"),
+                    Text("Time: ${widget.data['time']}"),
+                    Text("Value: ${widget.data['value']}€"),
+                    Text(
+                        "StartDate: ${dateFormat.format(widget.data['startDate'].toDate())}"),
+                    Text("isActive: ${widget.data['isActive']}"),
+                    Text("success: ${widget.data['success']}"),
+                  ],
+                )),
+            widget.data['images'] != null
+                ? SizedBox(
+                    height: 150,
+                    width: 300,
+                    child:
+                        ListView(scrollDirection: Axis.horizontal, children: [
+                      for (var image in widget.data['images'])
+                        Image.network(
+                          "$image",
+                          height: 150,
+                          width: 100,
+                        )
+                    ]))
+                : const Text("No Images uploaded yet"),
+            widget.data['videos'] != null
+                ? SizedBox(
+                    height: 150,
+                    width: 300,
+                    child:
+                        ListView(scrollDirection: Axis.horizontal, children: [
+                      // TODO how to make videoController dynamic so we can create this for each video maybe a list of controllers?
+                      SizedBox(
+                          height: 150,
+                          width: 150,
+                          child: VideoPlayerWidget(controller: videoController))
+                    ]))
+                : const Text("No Videos uploaded yet"),
+          ],
+        ),
       ),
     );
   }
