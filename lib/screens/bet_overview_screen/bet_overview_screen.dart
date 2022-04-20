83a792e43802f6bf5c7f6a9095ad06a4b5856183
Darjusch <Darjusch.schrand@code.berlin>
2022-04-20 15:17:22 +0200

    bet overview filter for history

--- lib/screens/bet_overview_screen/bet_overview_screen.dart
+++ lib/screens/bet_overview_screen/bet_overview_screen.dart
@@ -1,7 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitup/providers/bet_provider.dart';
 import 'package:fitup/utils/navigation_helper.dart';
 import 'package:fitup/utils/time_helper.dart';
-import 'package:fitup/widgets/app_bar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -23,91 +23,89 @@ class _BetOverviewScreenState extends State<BetOverviewScreen> {
 
   @override
   Widget build(BuildContext context) {
+    String userID = context.watch<User>().uid;
+
+    Provider.of<BetProvider>(context, listen: false).loadInitalBets(userID);
+    Provider.of<BetProvider>(context, listen: false).updateBetIsActive();
+    Provider.of<BetProvider>(context, listen: false).updateBetIsSuccessful();
     final activeBets = Provider.of<BetProvider>(context).getActiveBets();
     final inactiveBets = Provider.of<BetProvider>(context).getInactiveBets();
-    return Scaffold(
-      appBar: customAppBar(title: "Bet overview", context: context),
-      body: SizedBox(
-        height: 1000,
-        width: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        overview = true;
-                      });
-                    },
-                    child: const Text("Active Bets"),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        overview = false;
-                      });
-                    },
-                    child: const Text("Bet History"),
-                  ),
-                ],
-              ),
+
+    return SizedBox(
+      height: 1000,
+      width: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      overview = true;
+                    });
+                  },
+                  child: const Text("Active Bets"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      overview = false;
+                    });
+                  },
+                  child: const Text("Bet History"),
+                ),
+              ],
             ),
-            SizedBox(
-              height: 650,
-              width: double.infinity,
-              child: ListView.builder(
-                  itemCount: overview ? activeBets.length : inactiveBets.length,
-                  itemBuilder: (context, index) {
-                    final betItem =
-                        overview ? activeBets[index] : inactiveBets[index];
-                    return ListTile(
-                      tileColor: overview
-                          ? null
-                          : betItem.success
-                              ? Colors.green
-                              : Colors.red,
-                      onTap: () {
-                        NavigationHelper()
-                            .goToSingleBetScreen(betItem.betID, context);
-                      },
-                      minLeadingWidth: 0,
-                      leading: SizedBox(
-                        height: 25,
-                        width: 25,
-                        child: ImageIcon(
-                          AssetImage(actionToIcon[betItem.action]),
-                        ),
+          ),
+          SizedBox(
+            height: 600,
+            width: double.infinity,
+            child: ListView.builder(
+                itemCount: overview ? activeBets.length : inactiveBets.length,
+                itemBuilder: (context, index) {
+                  final betItem =
+                      overview ? activeBets[index] : inactiveBets[index];
+                  return ListTile(
+                    tileColor: overview
+                        ? null
+                        : betItem.success
+                            ? Colors.green
+                            : Colors.red,
+                    onTap: () {
+                      NavigationHelper()
+                          .goToSingleBetScreen(betItem.betID, context);
+                    },
+                    minLeadingWidth: 0,
+                    leading: SizedBox(
+                      height: 25,
+                      width: 25,
+                      child: ImageIcon(
+                        AssetImage(actionToIcon[betItem.action]),
                       ),
-                      title: Text(betItem.action),
-                      subtitle: InkWell(
-                        key: const ValueKey('betDetails'),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text("Time: ${betItem.time.format(context)}"),
-                            overview
-                                ? Text(
-                                    "Day: ${(betItem.duration - TimeHelper().betHasXDaysLeft(betItem.duration, betItem.startDate)) + 1} of ${betItem.duration}")
-                                : Text("Success: ${betItem.success}")
-                          ],
-                        ),
+                    ),
+                    title: Text(betItem.action),
+                    subtitle: InkWell(
+                      key: const ValueKey('betDetails'),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Time: ${betItem.time.format(context)}"),
+                          overview
+                              ? Text(
+                                  "Day: ${(betItem.duration - TimeHelper().betHasXDaysLeft(betItem.duration, betItem.startDate)) + 1} of ${betItem.duration}")
+                              : Text(betItem.success ? 'Success' : 'Failed')
+                        ],
                       ),
-                    );
-                  }),
-            ),
-          ],
-        ),
+                    ),
+                  );
+                }),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () => {
-                NavigationHelper().goToCreateBetScreen(context),
-              }),
     );
   }
 }
