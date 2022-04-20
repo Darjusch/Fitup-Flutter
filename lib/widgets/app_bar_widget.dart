d8424fd9e63e5e683d5d4d2c398dc27a27036fca
Darjusch <Darjusch.schrand@code.berlin>
2022-04-20 15:15:45 +0200

    logout bug fix

--- lib/widgets/app_bar_widget.dart
+++ lib/widgets/app_bar_widget.dart
@@ -1,6 +1,5 @@
 import 'package:fitup/providers/AuthenticationService.dart';
 import 'package:fitup/providers/bet_provider.dart';
-import 'package:fitup/utils/navigation_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -9,15 +8,9 @@ Widget customAppBar({String title, BuildContext context}) {
     title: Text(title),
     actions: <Widget>[
       IconButton(
-          onPressed: () => {
-                NavigationHelper().goToBetHistoryScreen(context),
-              },
-          icon: const Icon(Icons.history)),
-      IconButton(
         onPressed: () {
           Provider.of<BetProvider>(context, listen: false).removeAllBets();
-          Provider.of<Auth>(context, listen: false)
-              .signOut();
+          Provider.of<Auth>(context, listen: false).signOut();
           Navigator.of(context)
               .pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
         },
