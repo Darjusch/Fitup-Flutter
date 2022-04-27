47e87314f731009a93279235eb38c56de6b7f63f
Darjusch <Darjusch.schrand@code.berlin>
2022-04-27 15:30:39 +0200

    user provider is now selecting image

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitup/apis/firebase_api.dart';
+import 'package:fitup/controller/image_picker_helper.dart';
 import 'package:fitup/models/user_model.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserProvider extends ChangeNotifier {
   UserModel currentUser;
@@ -28,5 +31,17 @@ class UserProvider extends ChangeNotifier {
   void updateEmailAddress(String newEmail) {
     currentUser.email = newEmail;
     notifyListeners();
+  }
+
+  Future<String> uploadProfilePicFile(path, userID) async {
+    String result =
+        await FirebaseApi().uploadProfilePicFile(path, currentUser.userID);
+    return result;
+  }
+
+  Future<String> selectImage() async {
+    String path = await ImagePickerHelper()
+                        .getImageFrom(ImageSource.gallery);
+    return path;
   }
 }
